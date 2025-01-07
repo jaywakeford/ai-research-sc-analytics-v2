d59d1873a704c5cee33b3ed015f05e0c418de757
jaywakeford <jaywakeford@icloud.com>
2025-01-07 15:15:21 -0700

    Add type declarations for react-h5-audio-player and fix type errors

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import type { ReactElement } from 'react';
-import H5AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import H5AudioPlayer, { RHAP_UI, H5AudioPlayerProps } from 'react-h5-audio-player';
 import { getAudioPath } from '@/utils/paths';
 import 'react-h5-audio-player/lib/styles.css';
 
@@ -10,17 +10,6 @@ interface AudioPlayerProps {
   src: string;
   title?: string;
 }
-
-// Since we don't have type declarations for react-h5-audio-player,
-// we'll use a more generic type for the component
-type H5AudioPlayerProps = {
-  src: string;
-  autoPlay?: boolean;
-  showJumpControls?: boolean;
-  layout?: 'horizontal' | 'vertical' | 'stacked';
-  customProgressBarSection?: any[];
-  customControlsSection?: any[];
-};
 
 export default function AudioPlayer({ src, title }: AudioPlayerProps): ReactElement {
   return (
